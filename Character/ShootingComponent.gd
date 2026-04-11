57e9a557b9eae72bca6d9da3460fd717dcb21a1a
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2026-04-11 14:01:30 +0200

    Added shooting mechanic

--- Character/ShootingComponent.gd
+++ Character/ShootingComponent.gd
@@ -2,7 +2,28 @@ class_name ShootingComponent extends Node
 
 @export var InputManager : InputManagerComponent
 @export var BulletScene : PackedScene
+@export var BulletSpawn : Marker3D
+@export var BulletSpeed : float = 30
+@export var ShootSpeed : float = 100
+@export var FullAuto : bool = false
+
+var _last_shoot = 0
+var _previous_shoot_state : bool
 
 func _process(delta: float) -> void:
-	if not InputManager.ShootPressState:
-		pass
+	if InputManager.ShootPressState:
+		if FullAuto:
+			if _last_shoot + ShootSpeed < Time.get_ticks_msec():
+				var bullet : Bullet = BulletScene.instantiate()
+				get_tree().root.add_child(bullet)
+				bullet._velocity = InputManager.ForwardDirection * BulletSpeed
+				bullet.global_position = BulletSpawn.global_position
+				_last_shoot = Time.get_ticks_msec()
+		else:
+			if _previous_shoot_state != InputManager.ShootPressState:
+				var bullet : Bullet = BulletScene.instantiate()
+				get_tree().root.add_child(bullet)
+				bullet._velocity = InputManager.ForwardDirection * BulletSpeed
+				bullet.global_position = BulletSpawn.global_position
+				_last_shoot = Time.get_ticks_msec()
+	_previous_shoot_state = InputManager.ShootPressState
