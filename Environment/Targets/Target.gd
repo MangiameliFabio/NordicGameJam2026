7707a53509f344df721f2ddf9c7106b8fefe3039
Dominik Szczepaniak <dosz@itu.dk>
2026-04-11 23:29:51 +0200

    stumps added, dom scene hud and some targets added
    still needs MORE objects, gonna add later

--- Environment/Targets/Target.gd
+++ Environment/Targets/Target.gd
@@ -3,6 +3,8 @@ extends Node3D
 @export var ObjectToTrigger : Node
 @export var TargetArea : Area3D
 @export var DestroyAfterHit : bool = true
+@export var PointTarget : bool = false
+@export var AddedPoints : int = 30
 
 func _ready() -> void:
 	TargetArea.area_entered.connect(_on_area_entered)
@@ -10,7 +12,14 @@ func _ready() -> void:
 
 
 func _on_area_entered(area: Node3D) -> void:
+	if PointTarget:
+		Global.add_to_score(AddedPoints)
+		queue_free()
+		return
 	var trigger_object : Node = null
+	if not is_instance_valid(ObjectToTrigger):
+		return
+	
 	if ObjectToTrigger.has_method("Trigger"):
 		trigger_object = ObjectToTrigger
 	else:
